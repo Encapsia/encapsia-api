f9f8de0ab43396501a9aa805fa629da22393a925
Timothy Corbett-Clark <timothy.corbettclark@gmail.com>
2019-02-06 16:46:07 +0000

    Added credentials management and flake8

--- encapsia_api/__init__.py
+++ encapsia_api/__init__.py
@@ -1,1 +1,2 @@
 from encapsia_api.rest import EncapsiaApi, EncapsiaApiError  # NOQA
+from encapsia.credentials import CredentialsStore  # NOQA
---
+++ encapsia_api/credentials.py
@@ -0,0 +1,46 @@
+from pathlib import Path
+
+import toml
+
+
+class CredentialsStore:
+    CREDENTIALS_STORE = Path().home() / ".encapsia" / "credentials.toml"
+
+    def __init__(self):
+        # Create directory and file if they don't exist aleady
+        self.CREDENTIALS_STORE.parent.mkdir(mode=0o700, exist_ok=True)
+        self.CREDENTIALS_STORE.touch(mode=0o600, exist_ok=True)
+        self._store_timestamp = -1
+        self._store = None
+        self._refresh()
+
+    def _refresh(self):
+        current_timestamp = self.CREDENTIALS_STORE.stat().st_mtime
+        if self._store_timestamp < current_timestamp:
+            self._store = toml.loads(self.CREDENTIALS_STORE.read_text())
+            self._store_timestamp = current_timestamp
+
+    def _save(self):
+        self.CREDENTIALS_STORE.write_text(toml.dumps(self._store))
+        self._store_timestamp = self.CREDENTIALS_STORE.stat().st_mtime
+
+    def _get(self, label):
+        return self._store.get(label)
+
+    def get(self, label):
+        self._refresh()
+        return self._get(label)
+
+    def _set(self, label, host, token):
+        self._store[label] = {"host": host, "token": token}
+        self._save()
+
+    def set(self, label, host, token):
+        self._refresh()
+        self._set(label, host, token)
+
+    def remove(self, label):
+        self._refresh()
+        if label in self._store:
+            del self._store[label]
+        self._save()
