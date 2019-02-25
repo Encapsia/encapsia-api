a085ddd0199ef04aed4d472a6d23429c1470ac7a
Timothy Corbett-Clark <timothy.corbettclark@gmail.com>
2019-02-25 16:43:20 +0000

    Publish discover_credentials to top level and clock version

--- encapsia_api/__init__.py
+++ encapsia_api/__init__.py
@@ -1,10 +1,10 @@
 #: Keep in sync with git tag and package version in pyproject.toml.
-__version__ = "0.1.13"
+__version__ = "0.1.14"
 
 
 class EncapsiaApiError(RuntimeError):  # NOQA
     pass
 
 
-from encapsia_api.credentials import CredentialsStore  # NOQA
+from encapsia_api.credentials import CredentialsStore, discover_credentials  # NOQA
 from encapsia_api.rest import EncapsiaApi  # NOQA
