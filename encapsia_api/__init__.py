09f04d220ac9fccaf1bc7f9888de6baa9f46d8f6
Timothy Corbett-Clark <timothy.corbettclark@gmail.com>
2019-07-01 13:03:39 +0000

    Upgraded isort and fixed style

--- encapsia_api/__init__.py
+++ encapsia_api/__init__.py
@@ -6,6 +6,7 @@ class EncapsiaApiError(RuntimeError):  # NOQA
     pass
 
 
-from encapsia_api.credentials import CredentialsStore, discover_credentials  # NOQA
+from encapsia_api.credentials import CredentialsStore  # NOQA
+from encapsia_api.credentials import discover_credentials  # NOQA
 from encapsia_api.package import PackageMaker  # NOQA
 from encapsia_api.rest import EncapsiaApi  # NOQA
