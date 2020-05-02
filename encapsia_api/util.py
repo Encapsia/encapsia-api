c2222698dd5a7226b66e2453ab5cdf39507869d8
Timothy Corbett-Clark <timothy.corbettclark@gmail.com>
2020-05-02 16:51:00 +0000

    Refactor

--- encapsia_api/util.py
+++ encapsia_api/util.py
@@ -8,7 +8,7 @@ import arrow
 from .credentials import discover_credentials
 from .rest import EncapsiaApi
 
-__all__ = ["get_api_from_api_or_host", "make_uuid", "typed_csv_reader"]
+__all__ = ["get_api_from_api_or_host", "make_uuid"]
 
 
 def get_api_from_api_or_host(api_or_host):
@@ -23,43 +23,3 @@ def get_api_from_api_or_host(api_or_host):
 def make_uuid():
     """Generate and return new uuid typically used in encapsia."""
     return uuid.uuid4().hex
-
-
-def typed_csv_reader(data):
-    """Read CSV with typing info in the column names into a yielded dict per row."""
-    BOOLEAN_LOOKUP = {
-        "yes": True,
-        "y": True,
-        "t": True,
-        "true": True,
-        "no": False,
-        "n": False,
-        "f": False,
-        "false": False,
-    }
-    TYPE_CASTERS = {
-        "json": json.loads,
-        "integer": int,
-        "float": float,
-        "datetime": lambda x: arrow.get(x).datetime,
-        "boolean": lambda x: BOOLEAN_LOOKUP.get(x.lower()),
-    }
-    reader = csv.reader(io.StringIO(data))
-    raw_headers = next(reader)
-    headers = []
-    type_casters = {}
-    for i, header in enumerate(raw_headers):
-        name, *as_type = header.split("__", 1)
-        headers.append(name)
-        as_type = as_type[0] if as_type else None
-        caster = TYPE_CASTERS.get(as_type)
-        if caster:
-            type_casters[name] = caster
-    for row in reader:
-        row_as_dict = dict(zip(headers, row))
-        for name, caster in type_casters.items():
-            try:
-                row_as_dict[name] = caster(row_as_dict[name])
-            except ValueError:
-                row_as_dict[name] = None
-        yield row_as_dict
